f7f4222343247dcc3f4c33821bd202489d9e7507
nibchee <nirbhaybansal28@gmail.com>
2023-10-02 14:33:14 +0530

    User Microservice communicated with Rating Service

--- UserService/UserService/src/main/java/com/lcwd/user/service/impl/UserServiceImpl.java
+++ UserService/UserService/src/main/java/com/lcwd/user/service/impl/UserServiceImpl.java
@@ -1,12 +1,17 @@
 package com.lcwd.user.service.impl;
 
+import com.lcwd.user.service.entities.Rating;
 import com.lcwd.user.service.entities.User;
 import com.lcwd.user.service.exceptions.ResourceNotFoundException;
 import com.lcwd.user.service.repositories.UserRepository;
 import com.lcwd.user.service.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,7 +19,11 @@ import java.util.UUID;
 class UserServiceImpl implements UserService {
 
     @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
     private UserRepository userRepository;
+
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
     public User saveUser(User user) {
@@ -31,7 +40,13 @@ class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server!! : " + userId));
+        //get user from database with the help of user repository
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server!! : " + userId));
+        //fetch the rating of the above user from RATING SERVICE
+        ArrayList<Rating> ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/user/" + user.getUserId(), ArrayList.class);
+        logger.info("{}", ratingsOfUser);
+        user.setRatings(ratingsOfUser);
+        return user;
     }
 }
 
