61121c6713107cfe65e743cb8f160fb27ed6cd07
nibchee <nirbhaybansal28@gmail.com>
2023-10-02 18:18:28 +0530

    Adding FeignClinet for Rating Service

--- UserService/UserService/src/test/java/com/lcwd/user/service/UserServiceApplicationTests.java
+++ UserService/UserService/src/test/java/com/lcwd/user/service/UserServiceApplicationTests.java
@@ -1,7 +1,11 @@
 package com.lcwd.user.service;
 
+import com.lcwd.user.service.entities.Rating;
+import com.lcwd.user.service.external.services.RatingService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class UserServiceApplicationTests {
@@ -10,4 +14,11 @@ class UserServiceApplicationTests {
 	void contextLoads() {
 	}
 
+	@Autowired
+	private RatingService ratingService;
+
+	void createRating(){
+		Rating rating=Rating.builder().rating("10").userId("").hotelId("").feedback("this is created by feifgn client").build();
+		Rating rating1=ratingService.createRating(rating);
+	}
 }
